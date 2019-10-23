a4e1303fb4276ebbe089d3e382c3587ac8d4c9e8
Joseph Pyram <joseph.pyram@gmail.com>
2019-10-23 20:28:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
   # complexity.
   hash = {
           :railroads => {
-                          :pieces => 4
+                          :pieces => 4,
                           :rent_in_dollars => {
 	                                              :one_piece_owned => 25,
 	                                              :two_pieces_owned => 50,
